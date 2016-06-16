d0174c6a21a1eed3e539ac4bd84e3fb4a0911d2d
Ilya <IDolgopolov@bell-main.bellintegrator.ru>
2016-06-16 15:53:56 +0300

    lection 6 (navigations for iphone & ipad)

--- FaceIt/FaceViewController.swift
+++ FaceIt/FaceViewController.swift
@@ -62,17 +62,20 @@ class FaceViewController: UIViewController {
         expression.mouth = expression.mouth.sadderMouth()
     }
     
-    private var mouthCurvatures = [FacialExpression.Mouth.Frown: -1.0, .Grin:  0.5 , .Smile: 1.0, .Smirk: 0.5, .Neutral: 0.0]
+    private var mouthCurvatures = [FacialExpression.Mouth.Frown: -1.0, .Grin:  0.5 , .Smile: 1.0, .Smirk: -0.5, .Neutral: 0.0]
     private var eyeBrowTilts = [FacialExpression.EyeBrows.Furrowed: -0.5, .Relaxed: 0.5, .Normal: 0.0]
     
     private func updateUI () {
-        switch expression.eyes {
-        case .Open: faceView.eyeOpen = true
-        case .Closed: faceView.eyeOpen = false
-        case .Squinting: faceView.eyeOpen = false
+        if faceView != nil {
+            switch expression.eyes {
+            case .Open: faceView.eyeOpen = true
+            case .Closed: faceView.eyeOpen = false
+            case .Squinting: faceView.eyeOpen = false
+            }
+            faceView.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+            faceView.eyeBrowTilt = eyeBrowTilts[expression.eyeBrows] ?? 0.0
         }
-        faceView.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
-        faceView.eyeBrowTilt = eyeBrowTilts[expression.eyeBrows] ?? 0.0
+            
         
     }
     
